56c5fb71a3892380bc934ed09a2f9a5522ea6f65
HoodiNahi <jayesh.bhatt@ucdconnect.ie>
2025-07-17 04:30:02 +0100

    Added react src to go structure

---
+++ frontend/src/component/PliotSelector.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { fetchPilots } from '../api';
+
+export default function PilotSelector({ onSelect }) {
+  const [pilots, setPilots] = useState([]);
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    async function load() {
+      const res = await fetchPilots();
+      setPilots(res.data || []);
+    }
+    load();
+  }, []);
+
+  const handleChange = e => {
+    setValue(e.target.value);
+    onSelect(e.target.value);
+  };
+
+  return (
+    <div className="pilot-dropdown">
+      <select value={value} onChange={handleChange}>
+        <option value="" disabled>
+          Select a Pilot…
+        </option>
+        {pilots.map(p => (
+          <option key={p} value={p}>
+            {p}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
